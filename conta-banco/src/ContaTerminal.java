a5e0ec7ffc2cda9cd8c76889f8eec32fee9a5654
prof-leandro-teixeira <leandro_prof@yahoo.com>
2022-12-06 19:19:42 -0300

    Projeto finalizado.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int numeroConta = 0;
+        String agencia = "";
+        String nomeCliente = "";
+        double saldoConta = 0.00;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao Banco BNC!");
+        System.out.println("Vamos criar uma conta para você...");
+        System.out.println("Por favor, digite o nome do cliente:");
+        nomeCliente = s.next();
+        s.nextLine();
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = s.next();
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = s.nextInt();
+        System.out.println("Por favor, informe o valor de depósito para abertura da conta: ");
+        saldoConta = s.nextDouble();        
+
+        s.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco."+
+        " Sua agência é " +agencia+", conta " +numeroConta+ " e seu saldo"+
+        saldoConta+" já está disponível para saque.");
+
     }
 }
